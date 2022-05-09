1a744d1ef9c2c85707ad63c1967fa720eaf40986
spartha343 <spartha343@gmail.com>
2022-05-09 23:33:26 +0600

    11

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -5,15 +5,15 @@ const Blog = () => {
     return (
         <div>
             <Card body className='m-3 p-4'>
-                <h1>What is the difference between javascript and node.js</h1>
+                <h1>What is the difference between javascript and node.js ?</h1>
                 <p>js is a programming language on the other hand node js is an interpreter for js program </p>
             </Card>
             <Card body className='m-3 p-4'>
-                <h1> When should you use node.js and when should you use mongoDB</h1>
+                <h1> When should you use node.js and when should you use mongoDB ?</h1>
                 <p>mongo db is best for large application that doesn't need relational databse. and node js is most used for event-driven servers</p>
             </Card>
             <Card body className='m-3 p-4'>
-                <h1>What is the differences between SQL and noSQL databases.</h1>
+                <h1>What is the differences between SQL and noSQL databases ?</h1>
                 <p>SQL databases are table-based. SQL databases are vertically scalable. SQL databases are better for multi-row transactions. NoSQL databases are horizontally scalable.NoSQL databases are document, key-value, graph, or wide-column stores.NoSQL is better for unstructured data like documents or JSON. </p>
             </Card>
             <Card body className='m-3 p-4'>
